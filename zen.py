5d2d4cc4552b1c4562c34333c8846e081914d6b2
Linkan333 <linunybe@gmail.com>
2024-09-06 14:03:10 +0200

    Done with my first CS-Tool script in python

--- zen.py
+++ zen.py
@@ -2,9 +2,14 @@ import requests
 import time
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin, urlparse, urlunparse
+import sublist3r as sb
+import whois
+import scrapy
 
 blue = "\033[36m"
 reset = "\033[0m"
+OKGREEN = "\033[92m"
+WARNING = "\033[93m"
 
 def userInterfaceStartup():
     print(blue +"░▒▓████████▓▒░▒▓████████▓▒░▒▓███████▓▒░  ")
@@ -22,6 +27,9 @@ def userInterfaceStartup():
     print("░▒▓████████▓▒░▒▓████████▓▒░▒▓█▓▒░░▒▓█▓▒░ ")
     time.sleep(1)
     print(reset + "")
+    print(" ")
+    print(" ")
+    print(" ")
     print("Use -help for more information about the tool")
 
 def clean_url(url):
@@ -33,6 +41,41 @@ def clean_url(url):
         url_no_fragment = url_no_fragment.replace('http://', 'https://')
     
     return url_no_fragment
+
+
+def find_subdomain():
+
+    subdomain_input = input("Enter URL (example.com) > ")
+    time.sleep(2)
+    file_input = input("File name to save <FILENAME>.txt > ")
+
+    if not file_input.endswith(".txt"):
+        file_input += ".txt"
+
+    try: 
+            print("[*] Starting subdomain enumeration...")
+            domain = subdomain_input
+
+            
+            subdomains = sb.main(domain, 40, None, None, silent=False, verbose=True, enable_bruteforce=False, engines=None)
+
+            if subdomains:
+                with open(file_input, 'a') as file:
+                    for sub in subdomains:
+                        try:
+                            url = f"http://{sub}"
+                            response = requests.get(url, timeout=5) 
+                            file.write(f"Subdomain: {sub} - Status code: {response.status_code}\n")
+                            print(OKGREEN + f"[+] Subdomain {sub} is up with status {response.status_code}")
+                        except requests.exceptions.RequestException as e:
+                            file.write(f"Subdomain: {sub} - Error: {e}\n")
+                            print(WARNING + f"[!] Subdomain {sub} couldn't be reached. Error: {e}")
+                print(f"[*] Subdomains successfully saved to {file_input}")
+            else:
+                print(WARNING + "[!] No subdomains found or an error occurred")
+    except Exception as e:
+            print(WARNING + f"[!] Error enumerating the subdomains. Error: {e}")
+
 
 
 def inputs():
@@ -98,19 +141,60 @@ def inputs():
                 print("Error response code: " + r.status_code)
         except requests.exceptions.RequestException as e:
             print(f"Error checking the website {website_url}: error message: {e}")
-    if (user_input == 2):
-        print("Hello, you choosed Find subdomains")
-        return inputs()
-    if (user_input == 3):
-        print("Hello, you choosed WHOIS Lookup")
-        return inputs()
-    if (user_input == 4):
-        print("Hello, you choosed content Discovery")
-        return inputs()
-    if (user_input == 99):
-        print("Exiting...")
+    elif (user_input == 2):
+            find_subdomain()
+
+
+
+    elif (user_input == 3):
+
+        whois_url = input("WHOIS URL > ")
         time.sleep(2)
-        exit()
+        try:
+            w = whois.whois(whois_url)
+            print(w)
+        except Exception as e:
+            print(f"Something went wrong Error: {e}")
+
+
+
+    elif (user_input == 4):
+        webfuzzer_url = input("Discovery URL end with /DISCOVER >").strip('/')
+        wordlist_file = input("path/to/wordlist > ")
+        wordlist_save = input("File name to save to <FILENAME>.txt) > ")
+
+        
+
+        if not wordlist_file.endswith(".txt"):
+            print("Can't read this wordlist file, please use a (.txt) file.")
+        else:
+            try:
+                with open(wordlist_file, 'r') as file:
+                    wordlist = file.readlines()
+
+                    with open(wordlist_save, 'a') as result_file:
+                        for word in wordlist:
+                            word = word.strip()
+
+                            url = f'http://{webfuzzer_url}/{word}'
+
+                            try:
+                                response = requests.get(url, timeout=5)
+
+                                if response.status_code == 200:
+                                    result_file.write(f'[+] Discovery: {url} - Status Code: {response.status_code}\n')
+                                    print(OKGREEN + f"[+] Discovery {url} is up with status {response.status_code}")
+                                elif response.status_code == 404:
+                                    result_file.write(f'[!] Discovery: {url} - Status Code: {response.status_code}\n')
+                                    print(WARNING + f"[!] Discovery {url} couldn't be reached {response.status_code}")
+                            except requests.exceptions.RequestException as e:
+                                result_file.write(f'[!] Error discovering {url} - Error: {e}\n')
+                                print(WARNING + f"[!] Discovery {url} couldn't be reached Error: {e}")
+
+                    print(f"[*] Discovery results saved to {wordlist_save}")
+            except Exception as e:
+                print(WARNING + f"[!] Error discoverin ghte website. Error: {e}")
+      
 
 def print_help():
     print("Welcome to Zen")
